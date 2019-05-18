4156cbbdfb7bff7764994d31e2a8bb38d4b99985
dkkim <>
2019-05-19 03:23:43 +0900

    - 코틀린

---
+++ Bowling/app/src/main/java/com/dk/project/bowling/ui/activity/ScoreListActivity.kt
@@ -0,0 +1,13 @@
+package com.dk.project.bowling.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dk.project.bowling.R
+
+class ScoreListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score_list)
+    }
+}
