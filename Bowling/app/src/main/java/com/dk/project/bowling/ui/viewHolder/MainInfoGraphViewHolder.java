ed729c3ce52272694da504bdd69d8e15e6ba7023
dkkim <>
2019-07-04 21:17:53 +0900

    - 임시

--- Bowling/app/src/main/java/com/dk/project/bowling/ui/viewHolder/MainInfoGraphViewHolder.java
+++ Bowling/app/src/main/java/com/dk/project/bowling/ui/viewHolder/MainInfoGraphViewHolder.java
@@ -1,25 +1,29 @@
 package com.dk.project.bowling.ui.viewHolder;
 
 import android.view.View;
+import com.airbnb.lottie.LottieDrawable;
+import com.airbnb.lottie.TextDelegate;
 import com.dk.project.bowling.databinding.ViewHolderMainInfoGraphBinding;
 import com.dk.project.bowling.model.ScoreModel;
 import com.dk.project.post.base.BindViewHolder;
 
-
 public class MainInfoGraphViewHolder extends BindViewHolder<ViewHolderMainInfoGraphBinding, ScoreModel> {
 
-  public MainInfoGraphViewHolder(View itemView) {
-    super(itemView);
-  }
-
-  @Override
-  public void onBindView(ScoreModel item, int position) {
-    binding.scoreGraph.setBottomText("이번달 평균");
-    binding.scoreGraph.setMax(300);
-    if (item == null) {
-      binding.scoreGraph.setProgress(0);
-    } else {
-      binding.scoreGraph.setProgress(item.getAvgScore());
+    public MainInfoGraphViewHolder(View itemView) {
+        super(itemView);
     }
-  }
+
+    @Override
+    public void onBindView(ScoreModel item, int position) {
+        binding.scoreGraph.setBottomText("이번달 평균");
+        binding.scoreGraph.setMax(300);
+        if (item == null) {
+            binding.scoreGraph.setProgress(0);
+        } else {
+            binding.scoreGraph.setProgress(item.getAvgScore());
+        }
+
+        binding.scoreGraphLeft.setSpeed(0.9f);
+        binding.scoreGraphRight.setSpeed(0.9f);
+    }
 }
